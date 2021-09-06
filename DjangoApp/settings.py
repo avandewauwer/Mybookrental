2d7f49bfe411cbdd1abba487a2a2283e4f5ecef0
avandewauwer <annesophievandewauwer@gmail.com>
2021-09-06 13:47:57 +0200

    ec

--- DjangoApp/settings.py
+++ DjangoApp/settings.py
@@ -126,10 +126,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'project_name/static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'project_name/static')]
 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
-# DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DATABASES['default'].update(db_from_env)
