9eae238befd6c8eeab299a5f1e021d455edbf407
avandewauwer <annesophievandewauwer@gmail.com>
2021-09-06 16:38:46 +0200

    second

--- DjangoApi/models.py
+++ DjangoApi/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Books(models.Model):
+    title = models.CharField(max_length=255, unique=True)
+    author = models.CharField(max_length=255)
+    year = models.IntegerField
+    isbn = models.IntegerField
