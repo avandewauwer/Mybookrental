5c68196004636113589bcf86c9049970f149ff14
avandewauwer <annesophievandewauwer@gmail.com>
2021-09-07 11:00:54 +0200

    de

--- DjangoApp/settings.py
+++ DjangoApp/settings.py
@@ -52,6 +52,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.middleware.security.SecurityMiddleware',
 ]
 
 ROOT_URLCONF = 'DjangoApp.urls'
