a0bc44aa2ac5fd0f2a52576737f86476d732814a
avandewauwer <annesophievandewauwer@gmail.com>
2021-09-07 15:09:22 +0200

    ze

--- DjangoApi/views.py
+++ DjangoApi/views.py
@@ -1,9 +1,12 @@
+from rest_framework import permissions
 from DjangoApi.serializers import BookSerializer
 from rest_framework import generics, viewsets
 from .serializers import BookSerializer
 from .models import Book
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 
 class BookView(viewsets.ModelViewSet):
     serializer_class = BookSerializer
     queryset = Book.objects.all()
+    permissions_classes = [IsAuthenticatedOrReadOnly]
